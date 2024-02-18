a600164d1dfde74226af40c24a7c4c97bfe9fe4f
Nikolay Nechaev <Nikolay_Nechaev@mail.ru>
2024-02-18 15:24:57 +0300

    Server: record and show last measurement for each kind

--- server/main.py
+++ server/main.py
@@ -8,10 +8,20 @@ app = Flask(__name__)
 def index():
     return 'Nothing here'
 
+measurements = {}
+
 @app.route('/measure/<measurement>', methods=['POST'])
 def sensor(measurement: str):
-    print(request.get_json())
+    measurements[measurement] = request.get_json()
     return ''
+
+@app.route('/last/')
+def last_list():
+    return '/' + '<br>/'.join(measurements.keys())
+
+@app.route('/last/<measurement>')
+def last(measurement: str):
+    return measurements.get(measurement, '<no data for this measurement yet>')
 
 
 if __name__ == '__main__':
